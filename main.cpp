d5ead159fd581737f24760abf13b5e028680c5e7
Cristina-Diana Savin <cristinadianasavin@gmail.com>
2020-01-05 21:12:39 +0200

    Simplified vector management

--- main.cpp
+++ main.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
-//#include <random>
-
 
 using namespace std;
 
 int main()
 {
-    int dif, width=30, height=24, num_mines=200;
-    cout<<"DIFFICULTY: \n 1 = Beginner (9x9 - 10 Mines)  \n 2 = Medium (16x16 - 40 Mines) \n 3 = Difficult (30x16 - 99 Mines) \n 4 = Custom (max: 30x24 - 200 Mines) \n";
+    int dif, width, height, num_mines;
+    cout<<"DIFFICULTY: \n 1 = Beginner (9x9 - 10 Mines)  \n 2 = Medium (16x16 - 40 Mines) \n 3 = Difficult (16x30 - 99 Mines) \n 4 = Custom (max: 30x30 - 900 Mines) \n \n";
     cin>>dif;
+    cout<<endl;
     switch(dif) {
   case 1:
     width=9;
@@ -23,8 +22,8 @@ int main()
     num_mines=40;
     break;
   case 3:
-    width=30;
-    height=16;
+    width=16;
+    height=30;
     num_mines=99;
     break;
   case 4:
@@ -34,54 +33,43 @@ int main()
     cin>>height;
     cout<<"MINES = ";
     cin>>num_mines;
+    cout<<endl;
     break;
 }
-    int v[26][32], i, j, a, k=0, m, n, jm, im;
+    int v[32][32], i, j, k=0;
     srand (time(NULL));
+
+    for (i=0; i<height+2; i++)
+        for (j=0; j<width+2; j++)
+        if (i==0 || j==0 || i==height+1 || j==width+1) v[i][j]=0;
+        else
+           if (num_mines-k==0) v[i][j]=0;
+           else
+               if (rand()<(RAND_MAX*(num_mines-k)/(height*width-((i-1)*width+j))))
+               {
+                   v[i][j]=-1;
+                   k++;
+               }
+               else v[i][j]=0;
 
     for (i=1; i<=height; i++)
     {
         for (j=1; j<=width; j++)
-        {
-            a=rand();
-            cout<<a;
-           if (a<(RAND_MAX*(num_mines-k)/(height*width-((i-1)*width+j))))
-           {
-               v[i][j]=1;
-               k=k+1;
-           }
-           else v[i][j]=0;
-        }
-        cout<<endl;
-
-    }
-
-    for (jm=0; jm<32; jm++) //marginile iau valoarea 0
-    {
-        v[0][jm]=0;
-        for (int nmn=height+1; nmn<26; nmn++)
-        v[nmn][jm]=0;
-    }
-    for (im=1; im<=height; im++)
-    {
-        v[im][0]=0;
-        for (int nnn=width+1; nnn<32; nnn++)
-        v[im][nnn]=0;
-    }
-
-    for (m=1; m<=height; m++) //afisarea propriu-zisa
-    {
-        for (n=1; n<=width; n++)
-        {
-            if (v[m][n]==1) cout<<"* ";
-            else cout<<v[m-1][n-1]+v[m-1][n]+v[m-1][n+1]+v[m+1][n-1]+v[m+1][n]+v[m+1][n+1]+v[m][n-1]+v[m][n+1]<<" ";
-        }
+            if (v[i][j]==-1) cout<<"*  ";
+            else
+            {
+                for (short m=i-1; m<=i+1; m++)
+                    for (short n=j-1; n<=j+1; n++)
+                        if (v[m][n]==-1)
+                    v[i][j]=v[i][j]+1;
+                cout<<v[i][j]<<"  ";
+            }
         cout<<endl;
     }
 
-    cout<<endl<<k;
-
-
+    cout<<endl<<k<<endl;
 
     return 0;
 }
+
+
